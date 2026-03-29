c65120e2a926eee23ecf1738c0f3fafb9912e742
alison <alison.alves@academico.ifpb.edu.br>
2026-03-29 08:45:36 -0300

    iniciando

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ export function Navbar() {
     <nav className="navbar">
       <ul>
         <li><Link to="/">Início</Link></li>
-        <li><Link to="/professor/1">Alan Turing</Link></li>
-        <li><Link to="/professor/2">Ada Lovelace</Link></li>
-        <li><Link to="/professor/3">Grace Hopper</Link></li>
+        <li><Link to="/professor/1">Hugo Feitosa</Link></li>
+        <li><Link to="/professor/2">Andre Atanasio</Link></li>
+        <li><Link to="/professor/3">Alvoro Magnus</Link></li>
       </ul>
     </nav>
   );
